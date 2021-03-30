aa11276e23220185abd890d902586e79be74afb1
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-03-30 13:22:50 +0100

    Add products to cart

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import Home from "./pages/Home";
 import Result from "./pages/Result";
 import Product from "./pages/Product";
 import { loadStripe } from '@stripe/stripe-js'
-import { cartProvider } from 'use-shopping-cart';
+import { CartProvider } from 'use-shopping-cart';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 
@@ -20,13 +21,14 @@ function App() {
       currency="USD"
     >
     <BrowserRouter>
+    <Toaster position = "bottom-center" />
       <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/result" component={Result} />
       <Route path="/:productId" component={Product} />
       </Switch>
     </BrowserRouter>
-    <CartProvider />
+    </CartProvider>
     </QueryClientProvider>
   )
 }
